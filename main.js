75b8834008d5d743065fd629a3a174a5378d4229
Nathan Gordon Kirkpatrick <2283945k@student.gla.ac.uk>
2021-06-28 22:25:14 +0100

    scroll up button

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 window.onload = () => {
-    resetNavBar()
+    checkNavBar()
 }
 
 document.onscroll = () => {
-    resetNavBar()
+    checkNavBar()
+    checkUpArrow()
 }
 
-function resetNavBar() {
+document.getElementById('up-arrow').onclick = () => {
+    window.scrollTo(0,0)
+}
+
+function checkNavBar() {
     let navbar = document.getElementById('navbar')
     if (window.scrollY == 0) {
         navbar.style.backgroundColor = 'transparent'
@@ -14,5 +19,16 @@ function resetNavBar() {
     } else {
         navbar.style.backgroundColor = '#eee'
         navbar.style.boxShadow = '0 1px 15px rgba(0, 0, 0, .5)'
+    }
+}
+
+function checkUpArrow() {
+    let arrow = document.getElementById('up-arrow')
+    if (window.scrollY < (window.innerHeight / 2)) {
+        arrow.style.visibility = 'hidden'
+        arrow.style.opacity = '0'
+    } else {
+        arrow.style.visibility = 'visible'
+        arrow.style.opacity = '1'
     }
 }
