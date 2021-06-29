880f576aac628bb4cc9db0d88b2e093f20bfbf8d
Nathan Gordon Kirkpatrick <2283945k@student.gla.ac.uk>
2021-06-29 18:25:11 +0100

    responsive navbar

--- main.js
+++ main.js
@@ -1,19 +1,62 @@
+const navbar = document.getElementById('navbar')
+const navlinks = document.getElementById('nav-links')
+const contact = document.getElementById('contact-button')
+const arrow = document.getElementById('up-arrow')
+
 window.onload = () => {
-    checkNavBar()
+    checkNavBarVisibility()
+}
+
+window.onresize = () => {
+    checkNavBarVisibility()
+    
+    if (window.innerWidth <= 800 && navbar.style.height != "240px") {
+        hideNavItems()
+    } else {
+        showNavItems()
+        navbar.style.height = "50px"
+    }
 }
 
 document.onscroll = () => {
-    checkNavBar()
+    checkNavBarVisibility()
     checkUpArrow()
 }
 
 document.getElementById('up-arrow').onclick = () => {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
 }
 
-function checkNavBar() {
-    let navbar = document.getElementById('navbar')
-    if (window.scrollY == 0) {
+document.getElementById('hamburger').onclick = () => {
+    if (navbar.style.height == "240px") {
+        setTimeout(() => {
+            navbar.style.height = "50px"
+        }, 250)
+        hideNavItems()
+    } else {
+        navbar.style.height = "240px"
+        setTimeout(() => {
+            showNavItems()
+        }, 250)
+    }
+}
+
+function hideNavItems() {
+    navlinks.style.visibility = 'hidden'
+    navlinks.style.opacity = '0'
+    contact.style.visibility = 'hidden'
+    contact.style.opacity = '0'
+}
+
+function showNavItems() {
+    navlinks.style.visibility = 'visible'
+    navlinks.style.opacity = '1'
+    contact.style.visibility = 'visible'
+    contact.style.opacity = '1'
+}
+
+function checkNavBarVisibility() {
+    if (window.scrollY == 0 && window.innerWidth > 800) {
         navbar.style.backgroundColor = 'transparent'
         navbar.style.boxShadow = 'none'
     } else {
@@ -23,7 +66,6 @@ function checkNavBar() {
 }
 
 function checkUpArrow() {
-    let arrow = document.getElementById('up-arrow')
     if (window.scrollY < (window.innerHeight / 2)) {
         arrow.style.visibility = 'hidden'
         arrow.style.opacity = '0'
